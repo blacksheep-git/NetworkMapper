15730ce360db5278bb37b2fd354ddb142ebda727
mateo <m@teoo.iko>
2020-07-02 01:19:09 -0600

    Ping improvements

--- NetworkMapper.py
+++ NetworkMapper.py
@@ -2,28 +2,23 @@ import subprocess
 import sys
 
 def pingLan(subnet,start, end):
-    ipAddresses = []
+    string = "Pinging IP's from " + subnet + "." + str(start) + " to " + subnet + "." + str(end) + "...\n"
+    cmd = "echo \"" + string + "\" > results.txt "
+    ps = subprocess.call(cmd, shell=True)
+
     for ping in range(start, (end+1)):
            address = subnet + "." + str(ping)
-           cmd = "ping -c 1 " + address + " >> results.txt"
+           cmd = "ping -c 1 -w 2  " + address + " >> results.txt"
            ps = subprocess.call(cmd, shell=True)
            if ps == 0:
-               ipAddresses.append("Ping to " + address + "   SUCCESS")
+               print("Ping to " + address + "    SUCCESS")
            else:
-               ipAddresses.append("Ping to " + address + "   FAIL")
-
-    return ipAddresses
-
-def mapper(subnet,start, end):
-    print("Pinging IP's from " + subnet + "." + str(start) + " to " + subnet + "." + str(end) + " ...")
-    list = pingLan(subnet,start, end)
-    for i in list:
-        print(i)
-    print("Done.\n")
+               print("Ping to " + address + "    FAIL")
 
 def parseArgs(args):
     if len(args) > 1:
-        mapper(args[1], int(args[2]), int(args[3]))
+        print("Pinging IP's from " + args[1] + "." + args[2] + " to " + args[1] + "." + args[3] + "...")
+        pingLan(args[1], int(args[2]), int(args[3]))
     else:
         printUsage()
 
