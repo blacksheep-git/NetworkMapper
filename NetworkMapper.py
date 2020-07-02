40d0e550506869cffbb1a5df24751e8ed834a89f
mateo <m@teoo.iko>
2020-07-01 23:30:06 -0600

    lots of functional stuff

--- NetworkMapper.py
+++ NetworkMapper.py
@@ -1,11 +1,23 @@
 import subprocess
 
-for ping in range(1,10):
-	address = "127.0.0." + str(ping)
-	res = subprocess.call(['ping', '-c', '3', address])
-	if res == 0:
-		print( "ping to", address, "OK")
-	elif res == 2:
-		print("no response from", address)
-	else:
-		print("ping to", address, "failed!")
+ipAddresses = []
+
+
+def pingLan(subnet,start, end):
+    for ping in range(start, (end+1)):
+        address = subnet + "." + str(ping)
+        cmd = "ping -c 1 " + address + " > results.txt"
+        ps = subprocess.call(cmd, shell=True)
+        if ps <= 0:
+            ipAddresses.append(address)
+
+def mapper(subnet,start, end):
+    retVal = ""
+    pingLan(subnet,start, end)
+    for i in ipAddresses:
+        retVal += "ping to " + i + "   OK\n"
+    return retVal
+
+print(mapper("216.128.235", 1, 2))
+
+print(mapper("127.0.0", 1, 10))
