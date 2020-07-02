2781c98a5bd4dd435107c4ca8711370c1d2ad4db
mateo <m@teoo.iko>
2020-07-02 00:20:46 -0600

    lots of functional stuff

--- NetworkMapper.py
+++ NetworkMapper.py
@@ -1,34 +1,23 @@
 import subprocess
 
-
-
-def pingLan(live, subnet,start, end):
+def pingLan(subnet,start, end):
     ipAddresses = []
-    if live:
-        for ping in range(start, (end+1)):
-            address = subnet + "." + str(ping)
-            cmd = "ping -c 1 " + address + " >> results.txt"
-            ps = subprocess.call(cmd, shell=True)
-            if ps == 0:
-                ipAddresses.append("ping to " + address + "   SUCCESS")
-    else:
-        for ping in range(start, (end+1)):
-            address = subnet + "." + str(ping)
-            cmd = "ping -c 1 " + address + " >> results.txt"
-            ps = subprocess.call(cmd, shell=True)
-            if ps > 0:
-                ipAddresses.append("ping to " + address + "   FAIL")
+    for ping in range(start, (end+1)):
+           address = subnet + "." + str(ping)
+           cmd = "ping -c 1 " + address + " >> results.txt"
+           ps = subprocess.call(cmd, shell=True)
+           if ps == 0:
+               ipAddresses.append("Ping to " + address + "   SUCCESS")
+           else:
+               ipAddresses.append("Ping to " + address + "   FAIL")
 
     return ipAddresses
 
-def mapper(live,subnet,start, end):
-    print("These IP's from " + subnet + "." + str(start) + " -> " + subnet + "." + str(end) + " are live:")
-    list = pingLan(live, subnet,start, end)
+def mapper(subnet,start, end):
+    print("Pinging IP's from " + subnet + "." + str(start) + " to " + subnet + "." + str(end) + " ...")
+    list = pingLan(subnet,start, end)
     for i in list:
         print(i)
     print("\n")
 
-mapper(False, "216.128.235", 1, 33)
-mapper(True, "216.128.235", 1, 33)
-
-#print(mapper(True, "127.0.0", 1, 10))
+mapper("216.128.235", 1, 255)
